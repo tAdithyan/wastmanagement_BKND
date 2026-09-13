bf1c8aea673a22f4a9357273997fca2f67a66eaa
Adithyan-t <adithyan.essbee@outlook.com>
2026-09-13 09:37:34 +0530

    add agent details and bin route controllers with route preview functionality

--- src/routes/shift.routes.js
+++ src/routes/shift.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { listAgentDetails } from '../controllers/agent-details.controller.js';
 import { protect } from "../middlewares/auth.middleware.js";
 import { authorize } from "../middlewares/role.middleware.js";
 import { addShiftLocation, getActiveShift, listActiveAgentShifts, startShift, stopShift } from "../controllers/shift.controller.js";
 
 const router = Router();
+router.get('/agents', protect, authorize(['PERM_COLLECTION_READ']), listAgentDetails);
 router.get("/", protect, authorize(["PERM_COLLECTION_READ"]), listActiveAgentShifts);
 router.get("/active", protect, getActiveShift);
 router.post("/start", protect, startShift);
